abf2b04b4e047130816ff18fa72d8d8638ae0545
Alex-Ferreira-Santos <alexfstos@gmail.com>
2020-09-15 11:16:27 -0300

    template literals

---
+++ bundle/bundle8.js
@@ -0,0 +1,6 @@
+"use strict";
+
+var nome = "Alex";
+var idade = 17;
+console.log('Meu nome é ' + nome + ' e tenho ' + idade + ' anos');
+console.log("Meu nome \xE9 ".concat(nome, " e tenho ").concat(idade, " anos"));
---
+++ script/main8.js
@@ -0,0 +1,5 @@
+const nome ="Alex"
+const idade = 17
+
+console.log('Meu nome é ' + nome + ' e tenho ' + idade + ' anos')
+console.log(`Meu nome é ${nome} e tenho ${idade} anos`)
