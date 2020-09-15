2ff127cebc0dbdf665208d1c8d75ead3f77dd028
Alex-Ferreira-Santos <alexfstos@gmail.com>
2020-09-15 11:19:00 -0300

    object short syntax

--- bundle/bundle9.js
+++ bundle/bundle9.js
@@ -1,1 +1,11 @@
 "use strict";
+
+var nome = "Alex";
+var idade = 17; // não é necessario usar o nome:nome pois tem o mesmo nome, para simplificar pode-se apenas colocar o nome da variavel
+
+var usuario = {
+  nome: nome,
+  idade: idade,
+  cidade: "Rio"
+};
+console.log(usuario);
---
+++ script/main9.js
@@ -0,0 +1,10 @@
+const nome = "Alex"
+const idade = 17
+
+// não é necessario usar o nome:nome pois tem o mesmo nome, para simplificar pode-se apenas colocar o nome da variavel
+const usuario = {
+    nome,
+    idade,
+    cidade:"Rio"
+}
+console.log(usuario)
