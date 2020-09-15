af9a14c4b69fc5568cbfae648b98b33ce9644a3a
Alex-Ferreira-Santos <alexfstos@gmail.com>
2020-09-15 13:40:39 -0300

    exercicio2

--- exercicios/bundle/bundle2.js
+++ exercicios/bundle/bundle2.js
@@ -13,3 +13,24 @@ var usuarios = [{
   idade: 30,
   empresa: 'Facebook'
 }];
+var idade = usuarios.map(function (index) {
+  return index.idade;
+});
+console.log(idade);
+var filtro = usuarios.filter(function (item) {
+  return item.empresa === 'Rocketseat' && item.idade > 18;
+});
+console.log(filtro);
+var find = usuarios.find(function (item) {
+  return item.empresa === 'Google';
+});
+console.log(find);
+var multiplica = usuarios.map(function (value) {
+  value.idade = value.idade * 2;
+  return value;
+});
+var filtra = multiplica.filter(function (index) {
+  return index.idade <= 50;
+});
+console.log(multiplica);
+console.log(filtra);
--- exercicios/js/exercicio2.js
+++ exercicios/js/exercicio2.js
@@ -3,4 +3,31 @@ const usuarios = [
     { nome: 'Gabriel', idade: 15, empresa: 'Rocketseat' },
     { nome: 'Lucas', idade: 30, empresa: 'Facebook' },
 ];
-   
+
+const idade = usuarios.map(function(index){
+    return index.idade
+})
+console.log(idade)
+
+const filtro = usuarios.filter(function(item){
+    return item.empresa === 'Rocketseat' && item.idade>18
+})
+console.log(filtro)
+
+const find = usuarios.find(function(item){
+    return item.empresa === 'Google'
+})
+console.log(find)
+
+const multiplica = usuarios.map(function(value){
+    value.idade = value.idade*2
+    return value
+})
+const filtra = multiplica.filter(function(index){
+    return index.idade<=50
+})
+
+console.log(multiplica)
+console.log(filtra)
+
+
