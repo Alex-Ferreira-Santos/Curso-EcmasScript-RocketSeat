540cd18b60b7505cb6f402e8cafeb761f9e065b4
Alex-Ferreira-Santos <alexfstos@gmail.com>
2020-09-16 14:45:02 -0300

    exercicio 8

---
+++ exercicios/js/exercicio8.js
@@ -0,0 +1,7 @@
+import Usuario from "./function"
+import ClasseUsuario from "./function"
+import { idade } from "./function"
+import ClasseUser,{idade as IdadeUsuario} from "./function"
+
+ClasseUsuario.info()
+console.log(idade)
