bcd9cfae0139a4e3cd6e9bf330f9bf50a7773407
Alex-Ferreira-Santos <alexfstos@gmail.com>
2020-10-05 13:54:21 -0300

    renderizando em tela

--- src/main.js
+++ src/main.js
@@ -1,87 +1,49 @@
-import axios from 'axios';
+class App{
+    constructor(){
+        this.repositories = []
+        this.formEl = document.getElementById('repo-form')
+        this.listEl = document.getElementById('repo-list')
+        this.registerHandlers()
+    }
+    registerHandlers(){
+        this.formEl.onsubmit = event => this.addRepository(event)
+    }
+    addRepository(event){
+        event.preventDefault()
+        this.repositories.push({
+            name:'rocketseat.com.br',
+            description:'Tire a sua ideia do papel e dê vida à sua startup',
+            avatar_url:'https://avatars0.githubusercontent.com/u/28929274?v=4',
+            html_url:'http://github.com/rocketseat/rocketseat.com.br',
+        })
 
-const minhaPromise = () => new Promise((resolve, reject) =>
-    setTimeout(() => { resolve("OK") }, 2000))
+        this.render()
+    }
+    render(){
+        this.listEl.innerHTML=''
+        this.repositories.forEach( repo => {
 
-/*async function executaPromise(){
-    console.log(await minhaPromise())
-    console.log(await minhaPromise())
-    console.log(await minhaPromise())
-}
+            let imgEl = document.createElement('img')
+            imgEl.setAttribute('src', repo.avatar_url)
 
-const executaPromise = async () => {
-    console.log(await minhaPromise())
-    console.log(await minhaPromise())
-    console.log(await minhaPromise())
-}
+            let titleEl = document.createElement('strong')
+            titleEl.appendChild(document.createTextNode(repo.name))
 
-executaPromise()*/
+            let descriptionEl = document.createElement('p')
+            descriptionEl.appendChild(document.createTextNode(repo.description))
 
-class Api {
-    static async getUserInfo(username) {
-        try {
-            const response = await axios.get(`https://api.github.com/users/${username}`)
-            console.log(response)
-        } catch (err) {
-            console.warn("Erro na API")
-        }
+            let linkEl = document.createElement('a')
+            linkEl.setAttribute('target','_blank')
+            linkEl.appendChild(document.createTextNode('Acessar'))
 
+            let listItemEl = document.createElement('li')
+            listItemEl.appendChild(imgEl)
+            listItemEl.appendChild(titleEl)
+            listItemEl.appendChild(descriptionEl)
+            listItemEl.appendChild(linkEl)
+
+            this.listEl.appendChild(listItemEl)
+        })
     }
 }
-
-Api.getUserInfo('diego3g')
-
-//======================================EXERCICIOS=======================================
-
-// Funão delay aciona o .then após 1s
-const delay = (value) => new Promise(resolve => setTimeout(()=>{resolve(value)}, 1000));
-async function umPorSegundo() {
-    console.log(await delay("1s"))
-    console.log(await delay("2s"))
-    console.log(await delay("3s"))
-}
-umPorSegundo();
-
-
-
-async function getUserFromGithub(user) {
-    try{
-        const response = await axios.get(`https://api.github.com/users/${user}`)
-        console.log(response.data);
-    }
-    catch(err){
-            console.warn('Usuário não existe');
-    }
-}
-getUserFromGithub('diego3g');
-getUserFromGithub('diego3g124123');
-
-
-class Github {
-    static async getRepositories(repo) {
-        try{
-            const response = await axios.get(`https://api.github.com/repos/${repo}`)
-            console.log(response.data);
-        }
-        catch(err){
-            console.warn('Repositório não existe');
-        }
-    }
-}
-Github.getRepositories('rocketseat/rocketseat.com.br');
-Github.getRepositories('rocketseat/dslkvmskv');
-
-
-
-
-
-const buscaUsuario = async usuario => {
-    try{
-        const response = await axios.get(`https://api.github.com/users/${usuario}`)
-        console.log(response.data);
-    }
-    catch(err){
-        console.log('Usuário não existe');
-    }
-}
-buscaUsuario('diego3g');
+new App()
